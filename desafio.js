28f9c6f25855cb9af86f35b52ac760ef625f2e8b
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-12 11:53:01 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,11 @@
+//🔴 Giovana, Alice, Wellington
+//🟢 Thays, Gabriella
+//🔵 Ryan, Nicolas, Arthur A., Arthur O., Lucas
+//⚫️ Hugo, Nathielly e Emilly
+
+//🔴Declarar 5 variáveis para cadastrar alunos
+//🟢Declarar 5 variáveis de um produto de uma loja
+//🔵Declarar 5 variáveis para um perfil de um jogador
+//⚫️Declarar 5 variáveis para um funcionário de uma empresa
+
+//Fazer uma saída interpolada
