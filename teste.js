2085d965b9f2d8a27b3344061eac0c9989238001
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-12 11:38:05 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,13 @@
+const destino = "Foz do Iguaçu";
+const origem = "Curitiba";
+const nomeViajante = "Luiza";
+let duracao = 8;
+let transporte = "carro";
+
+console.log(`${nomeViajante} viajará de ${origem} para ${destino} por ${duracao}h de ${transporte}.`);
+
+//NÃO COPIAR 🔴🟢🔵⚫️
+//🔴 Giovana, Alice, Wellington
+//🟢 Thays, Gabriella
+//🔵 Ryan, Nicolas, Arthur A., Arthur O., Lucas
+//⚫️ Hugo, Nathielly e Emilly
