67015b288d90d1e7dda34051efee43c60727c19b
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-13 12:42:07 +0000

    versao4

--- desafio.js
+++ desafio.js
@@ -1,11 +1,26 @@
-//🔴 Giovana, Alice, Wellington
-//🟢 Thays, Gabriella
+//🔴 Giovana, Alice, Wellington, Alexia, Jonathas, Luis
+//🟢 Thays, Gabriella, Maria, Brenda, Yuri
 //🔵 Ryan, Nicolas, Arthur A., Arthur O., Lucas
-//⚫️ Hugo, Nathielly e Emilly
+//⚫️ Hugo, Nathielly e Emilly, Mariana
 
-//🔴Declarar 5 variáveis para cadastrar alunos
-//🟢Declarar 5 variáveis de um produto de uma loja
-//🔵Declarar 5 variáveis para um perfil de um jogador
-//⚫️Declarar 5 variáveis para um funcionário de uma empresa
+//🔴1. Declarar 5 variáveis para cadastrar alunos e console.log
+//🔴2. Crie duas variáveis numéricas representando duas notas, imprima o valor em um console.log
+//🔴3. Adicione as notas de prova e trabalho e calcule a média final somando as notas, dividindo pela quantidade de notas.
+
+//🟢1. Declarar 5 variáveis de um produto de uma loja e console.log
+//🟢2. Crie duas variáveis numéricas representando o preço original e o desconto, imprima o valor em um console.log
+//🟢3. Calcule o valor com 20% (0.20) de desconto, quantos produtos inteiros se compram com R$ 500,00 reais e quanto sobra de troco.
+
+//🔵1. Declarar 5 variáveis para um perfil de um jogador e console.log
+//🔵2. Crie duas variáveis numéricas com os pontos da fase 1 e fase 2, imprima o valor em um console.log
+//🔵3. Calcule o total de pontos, diferença entre fases, nível atual e pontos faltando para o próximo nível.
+
+//⚫️1. Declarar 5 variáveis para um funcionário de uma empresa e console.log
+//⚫️2. Crie duas variáveis numéricas com o salário base e o bônus mensal, imprima o valor em um console.log
+//⚫️3. Calcule o salário total e INSS(desconto) de 10% (0.10)
 
 //Fazer uma saída interpolada
+
+//git add desafio.js
+//git commit -m "versao3"
+//git push
