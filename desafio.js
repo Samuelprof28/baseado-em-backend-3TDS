aba58c5b488d25d18a3b8b9335528cb5ae2f5070
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-05-18 11:55:25 +0000

    versao23

--- desafio.js
+++ desafio.js
@@ -6,18 +6,22 @@
 //🔴1. Declarar 5 variáveis para cadastrar alunos e console.log
 //🔴2. Crie duas variáveis numéricas representando duas notas, imprima o valor em um console.log
 //🔴3. Adicione as notas de prova e trabalho e calcule a média final somando as notas, dividindo pela quantidade de notas.
+//🔴4. Um aluno é aprovado se a média maior ou igual a 6 e com a frequência acima de 75. Exiba se está aprovado ou se precisa de recuperação ou se está reprovado (por nota ou por frequência).
 
 //🟢1. Declarar 5 variáveis de um produto de uma loja e console.log
 //🟢2. Crie duas variáveis numéricas representando o preço original e o desconto, imprima o valor em um console.log
 //🟢3. Calcule o valor com 20% (0.20) de desconto, quantos produtos inteiros se compram com R$ 500,00 reais e quanto sobra de troco.
+//🟢4. Declare preço e quantidade, calcule o total com o valor do desconto e quantos produtos inteiros se compram com R$500,00 e por último quanto sobra de troco.
 
 //🔵1. Declarar 5 variáveis para um perfil de um jogador e console.log
 //🔵2. Crie duas variáveis numéricas com os pontos da fase 1 e fase 2, imprima o valor em um console.log
 //🔵3. Calcule o total de pontos, diferença entre fases, nível atual e pontos faltando para o próximo nível.
+//🔵4. Declare níveis e pontos, exiba uma pontuação cujo o nível está de acordo com um alto padrão e se será banido se a pontuação for baixa.
 
 //⚫️1. Declarar 5 variáveis para um funcionário de uma empresa e console.log
 //⚫️2. Crie duas variáveis numéricas com o salário base e o bônus mensal, imprima o valor em um console.log
 //⚫️3. Calcule o salário total e INSS(desconto) de 10% (0.10)
+//⚫️4. Declare anos de Empresa e salário e exiba se recebe promoção ou se a experiência dele é sênior, pleno ou junior
 
 //Fazer uma saída interpolada
 
